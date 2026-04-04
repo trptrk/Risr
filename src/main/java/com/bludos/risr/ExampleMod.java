dca07fcf11b10f96e3b2c0b688c2bda8ae6681fb
BludosDev <bludosofficial+67@gmail.com>
2026-04-04 14:48:34 +0530

    Initial commit

---
+++ src/main/java/com/bludos/risr/ExampleMod.java
@@ -0,0 +1,10 @@
+package com.bludos.risr;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ExampleMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("Hello Fabric world!");
+    }
+}
